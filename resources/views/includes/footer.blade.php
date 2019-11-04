3a28a61671380806d069123b37be13ae7e75d3e1
aungtnaing82@gmail.com <aungtnaing82@gmail.com>
2019-11-04 13:48:38 +0630

    new add ed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
-				
+
 				<!-- 	<p>Spiral Architects is a full serviced architectural, urban planning, structural engineering and interior design firm based in Myanmar. The company was established in 2008 by Mr. Rui Barreto.</p>
  -->					<!-- <ul class="social-icons">
 						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
@@ -11,9 +11,9 @@
 						<li><a href="#"><i class="fa fa-behance"></i></a></li>
 					</ul> -->
 				</div>
-			
+
 			<div class="col-md-5">
-				
+
 
 
 					<div class="map-section">
@@ -26,23 +26,23 @@
 						</div>
 					</div>
 
-				
+
 				</div>
 
 
 				<div class="col-md-3">
-					<div class="contact-info">	
+					<div class="contact-info">
 						<h6>Contact Info</h6>
 						<p>spiral architects</p>
 						<ul class="contact-list">
-							<li><span>Phone:</span> +95 (1) 661556</li>
-							<li> +95 (1) 660809</li>
+							<li><span>Phone:</span></li>
+							<!-- <li> +95 (1) 661556 +95 (1) 660809</li> -->
 							<li> +95 (9) 50.12.477</li>
 
-							<li><span>Address:</span> 43, Corner of Parami Road & Mya Sabi Street 8 Ward, Yankin Township, Yangon, Myanmar</li>
+							<li><span>Address:</span> No. 25E, Thiri Mingalar Yeikthar Lane, Kabar Aye Pagoda Rd, Yankin Township, Yangon, Myanmar</li>
 							<li><span>Email:</span> <a href="#">info@spiralarchitectsmyanmar.com</a></li>
 							<li><span>Website:</span> <a href="#">www.spiralarchitectsmyanmar.com</a></li>
-							
+
 
 						</ul>
 					</div>
@@ -50,7 +50,7 @@
 
 			</div>
 			<div class="row">
-				<div class="col-md-12">	
+				<div class="col-md-12">
 					<div class="copyright-text">
 						<p>@ 2017 Info-Link IT Solution. All Rights Reserved.</p>
 					</div>
--- resources/views/includes/footermyanmar.blade.php
+++ resources/views/includes/footermyanmar.blade.php
@@ -3,12 +3,12 @@
 		<div class="row">
 			<div class="col-md-4">
 				<div class="about-us">
-				
+
 					<p><em>Spiral ဗိသုကာသည္ </em> ျမန္မာႏိုင္ငံတြင္ အေျခစိုက္ေသာ အေဆာက္အဦႏွင္႔ ျမိဳျပစီမံကိန္းမ်ားအတြက္ လိုအပ္သည္႔ ၀န္ေဆာင္မႈလုပ္ငန္းမ်ားျဖစ္ေသာ ဗိသုကာပံုစံ၊ ျမိဳ႕ျပစီမံကိန္းေရးဆြဲျခင္း၊ အေဆာက္အဦ အင္ဂ်င္နီယာႏွင္႔ အိမ္တြင္းအလွဆင္ျခင္းမ်ား အားလံုးကို တစုတစည္းတည္းလုပ္ကိုင္ေသာ အဖြဲ႕အစည္းတစ္ခုျဖစ္ပါသည္။</p>
-					
+
 				</div>
 			</div>
-			
+
 			<div class="col-md-5">
 
 				<div class="map-section">
@@ -21,28 +21,30 @@
 					</div>
 				</div>
 
-				
+
 			</div>
 
 
 
 			<div class="col-md-3">
-				<div class="contact-info">	
+				<div class="contact-info">
 					<h6>ဆက္သြယ္ရန္အခ်က္အလက္</h6>
 					<p>spiral architects</p>
 					<ul class="contact-list">
-						<li><span>ဖုန္​း:</span> + ၉၅ ( ၁ ) ၆၆၁၅၅၆</li>
-						<li><span>လိပ္​စာ:</span> ၄၃, ပါရမီလမ္​း​ေထာင္​့ , ျမစပါယ္​လမ္​း , ၈ ရပ္​ကြက္​ , ရန္​ကင္​းၿမိဳ႕နယ္​ , ရန္​ကုန္​ , ျမန္​မာ</li>
+						<li><span>ဖုန္​း:</span> +95 (9) 50.12.477</li>
+						<!-- <li> + ၉၅ ( ၁ ) ၆၆၁၅၅၆</li> -->
+						<!-- <li><span>လိပ္​စာ:</span> ၄၃, ပါရမီလမ္​း​ေထာင္​့ , ျမစပါယ္​လမ္​း , ၈ ရပ္​ကြက္​ , ရန္​ကင္​းၿမိဳ႕နယ္​ , ရန္​ကုန္​ , ျမန္​မာ</li> -->
+						<li><span>လိပ္​စာ:</span> အမွတ္ ( ၂၅ / E ) သီရိမဂၤလာရိပ္သာလမ္းသြယ္ ကမၻာေအး ဘုရားလမ္း ရန္ကင္းၿမိဳ႕နယ္ ရန္ကုန္ၿမိဳ႕</li>
 						<li><span>Email:</span> <a href="#">spiral.rb@gmail.com</a></li>
 					</ul>
 
 				</div>
 			</div>
 
-			
+
 		</div>
 		<div class="row">
-			<div class="col-md-12">	
+			<div class="col-md-12">
 				<div class="copyright-text">
 					<p>@ 2017 Info-Link IT Solution. All Rights Reserved.</p>
 				</div>
